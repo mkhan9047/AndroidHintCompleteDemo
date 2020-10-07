a95495918d96d80ab49aae5056a44f388b855223
almujahidkhan <almujahid1100@gmail.com>
2020-10-07 19:12:46 +0600

    demo done

--- app/src/main/java/com/app/androidhintcompletedemo/MainActivity.kt
+++ app/src/main/java/com/app/androidhintcompletedemo/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.app.androidhintcompletedemo
 
+import android.graphics.Color.argb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.DecelerateInterpolator
+import android.widget.Toast
+import android.widget.Toast.makeText
+import com.takusemba.spotlight.OnSpotlightListener
+import com.takusemba.spotlight.OnTargetListener
+import com.takusemba.spotlight.Spotlight
+import com.takusemba.spotlight.Target
+import com.takusemba.spotlight.effet.RippleEffect
+import com.takusemba.spotlight.shape.Circle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        start_spot?.setOnClickListener {
+            val viewsList = ArrayList<View>()
+            HintUtils.showHintsToUser(this,
+                R.layout.layout_target,
+                btn_one, btn_two, btn_three, btn_four
+            )
+        }
     }
+
 }
